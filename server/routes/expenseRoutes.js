06cf31a3d1a1ba9e57e9afe25d484a01f53bd46f
FonzAye <george_super@ukr.net>
2025-02-18 14:46:36 +0200

    wtf

---
+++ server/routes/expenseRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const expenseController = require('../controllers/expenseController');
+
+const router = express.Router();
+
+router.post('/', expenseController.addExpense);
+router.get('/user/:user_id', expenseController.getExpensesByUser);
+
+module.exports = router;
